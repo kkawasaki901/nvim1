a5a4f7fbc0ec63edc72c571cd75ba76146afe9fe
kawasaki901 <kkawasaki901@gmail.com>
2025-12-28 19:52:12 +0900

    aerialを除外

--- org-init.lua
+++ org-init.lua
@@ -557,33 +557,6 @@ require("lazy").setup(
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
-
-  -- HACK: aerial.nvimプラグイン（コードアウトライン表示）
-  {
-    'stevearc/aerial.nvim',
-    opts = {},
-    -- Optional dependencies
-    dependencies = {
-       "nvim-treesitter/nvim-treesitter",
-       "nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-      require("aerial").setup({
-        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-
-        --[[
-        -- カーソル移動はいらないのでコメントアウト
-        on_attach = function(bufnr)
-          -- Jump forwards/backwards with '{' and '}'
-          vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-          vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-        end,
-        --]]
-      })
-      -- You probably also want to set a keymap to toggle aerial
-      vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
-    end,
-  },
   
     -- HACK: Treesitter
   {
@@ -664,8 +637,6 @@ require("lazy").setup(
 
   },
   
-
-
 
 }, -- end of plugins table
 
---
+++ 動くやつ/aerial.lua
@@ -0,0 +1,26 @@
+-- HACK: aerial.nvimプラグイン（コードアウトライン表示）
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+       "nvim-treesitter/nvim-treesitter",
+       "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+      require("aerial").setup({
+        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+
+        --[[
+        -- カーソル移動はいらないのでコメントアウト
+        on_attach = function(bufnr)
+          -- Jump forwards/backwards with '{' and '}'
+          vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+          vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+        end,
+        --]]
+      })
+      -- You probably also want to set a keymap to toggle aerial
+      vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+    end,
+  },
